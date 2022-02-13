3918ed68432226cead6b26d7f861773679032cc1
Ben <e0310113@u.nus.edu>
2022-02-13 16:11:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,8 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 db = SQLAlchemy(app)
-CORS(app)
 
 @app.route('/hello')
 def hello():
