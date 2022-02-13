8683f6d925859ec210721a71541e105437143117
Ben <e0310113@u.nus.edu>
2022-02-13 16:07:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,51 +13,52 @@ CORS(app)
 @app.route('/hello')
 def hello():
   return "Hello World!"
-# ## Item
-# class Item(db.Model):
-#   id = db.Column(db.Integer, primary_key=True)
-#   title = db.Column(db.String(80), unique=True, nullable=False)
-#   content = db.Column(db.String(120), unique=True, nullable=False)
 
-#   def __init__(self, title, content):
-#     self.title = title
-#     self.content = content
+## Item
+class Item(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(80), unique=True, nullable=False)
+  content = db.Column(db.String(120), unique=True, nullable=False)
 
-#   @app.route('/items/<id>', methods=['GET'])
-#   def get(id):
-#     item = Item.query.get(id)
-#     del item.__dict__['_sa_instance_state']
-#     return jsonify(item.__dict__)
+  def __init__(self, title, content):
+    self.title = title
+    self.content = content
 
-#   @app.route('/items', methods=['GET'])
-#   def get_items():
-#     items = []
-#     for item in db.session.query(Item).all():
-#       del item.__dict__['_sa_instance_state']
-#       items.append(item.__dict__)
-#     print(items)
-#     return jsonify(items)
+  @app.route('/items/<id>', methods=['GET'])
+  def get(id):
+    item = Item.query.get(id)
+    del item.__dict__['_sa_instance_state']
+    return jsonify(item.__dict__)
 
-#   @app.route('/items', methods=['POST'])
-#   def create_item():
-#     body = request.get_json()
-#     db.session.add(Item(body['title'], body['content']))
-#     db.session.commit()
-#     return "item created"
+  @app.route('/items', methods=['GET'])
+  def get_items():
+    items = []
+    for item in db.session.query(Item).all():
+      del item.__dict__['_sa_instance_state']
+      items.append(item.__dict__)
+    print(items)
+    return jsonify(items)
 
-#   @app.route('/items/<id>', methods=['PUT'])
-#   def update_item(id):
-#     body = request.get_json()
-#     db.session.query(Item).filter_by(id=id).update(
-#       dict(title=body['title'], content=body['content']))
-#     db.session.commit()
-#     return "item updated"
+  @app.route('/items', methods=['POST'])
+  def create_item():
+    body = request.get_json()
+    db.session.add(Item(body['title'], body['content']))
+    db.session.commit()
+    return "item created"
 
-#   @app.route('/items/<id>', methods=['DELETE'])
-#   def delete_item(id):
-#     db.session.query(Item).filter_by(id=id).delete()
-#     db.session.commit()
-#     return "item deleted"
+  @app.route('/items/<id>', methods=['PUT'])
+  def update_item(id):
+    body = request.get_json()
+    db.session.query(Item).filter_by(id=id).update(
+      dict(title=body['title'], content=body['content']))
+    db.session.commit()
+    return "item updated"
+
+  @app.route('/items/<id>', methods=['DELETE'])
+  def delete_item(id):
+    db.session.query(Item).filter_by(id=id).delete()
+    db.session.commit()
+    return "item deleted"
 
 # ## attendance
 # class Attendance(db.Model):
@@ -222,7 +223,7 @@ def hello():
 #     return "bird deleted"
 
 
-# db.create_all()
+db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
